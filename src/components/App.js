bfe10b5a694555a187c00f5308a81fee94e9f67e
Rancor38 <zak.rancor@hotmail.com>
2023-05-01 09:34:11 -0700

    completed app

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Main from '../pages/Main/Main';
 import StarshipsList from '../pages/StarshipsList/StarshipsList';
 import Starship from '../pages/Starship/Starship';
 import Nav from './Nav';
+import Four04 from '../pages/404/Four04'
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
         <Route path="/ships" element={<StarshipsList />} />
         
           <Route path="/ships/:id" element={<Starship />} />
+
+          <Route path="*" element={<Four04 />} />
 
         </Routes>
       </main>
---
+++ src/pages/404/Four04.js
@@ -0,0 +1,7 @@
+const Four04 = () => {
+    return (
+        <h1>No Page Found</h1>
+    );
+};
+
+export default Four04;
